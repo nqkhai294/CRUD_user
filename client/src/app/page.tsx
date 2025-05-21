30e26f555a01c61afe85c79c0e18f4d2f763401d
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2025-05-21 23:05:04 +0700

    Initial commit: fullstack with client and server

---
+++ client/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        Hello World!
+      </div>
+
+      <Button type="primary"> 
+        Ant Design Button
+      </Button>
+    </div>
+  );
+}
